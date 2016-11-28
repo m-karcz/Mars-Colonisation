8ddd4f02b7cc32e9d85b09ce750c5eb82e90a8cc
Mariusz Karcz <ujemnypozyton@gmail.com>
2016-11-28 20:23:21 +0100

    Nei_gen::is_new() fixed, everything working

--- ConsoleApplication1/ConsoleApplication1/Graph.hpp
+++ ConsoleApplication1/ConsoleApplication1/Graph.hpp
@@ -11,6 +11,8 @@
 typedef std::vector<std::vector<bool> > adj_matrix;
 struct Graph
 { //graf
+	Graph();
+	Graph(System::Drawing::Image^ img);
 	std::vector<std::vector<Point>> points;
 	void clear(); //ustawia costy w pointsach
 	adj_matrix get_adjacency_matrix(Solution & actual);
