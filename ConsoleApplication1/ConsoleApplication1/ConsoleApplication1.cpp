6b59616c463080110ac1e13de545f11cb4ad7b58
Mieszko <Mieszko@DESKTOP-D5L3DLV>
2016-12-05 20:15:37 +0100

    First "working" version. Max_range set in SA::run <-- change it.

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -22,9 +22,6 @@ int main(array<System::String ^> ^args)
 	//WinExec("matlab -r test", SW_SHOWNORMAL);
 	std::srand(static_cast<unsigned int>(std::time(nullptr)));
     Console::WriteLine(L"tutaj bedzie latwo debugowac, np. coutem");
-
-
-
 	ConsoleApplication1::MainForm myForm;
 	myForm.ShowDialog();
 	Console::ReadKey();
